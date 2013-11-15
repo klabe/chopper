ba1fc1c73d245f1f15c95a091c37b78409c0e319
Kevin Labe <klabe@uchicago.edu>
2013-11-14 22:25:01 -0600

    Now correctly reads times.

--- Makefile
+++ Makefile
@@ -4,6 +4,8 @@ CC = g++
 
 CHOPPER_SOURCES = chopper.cpp PZdabFile.cxx 
 
+# PThread.cxx PThreadInt.cxx PZdabWriter.cxx CUtils.cxx error_ph.c
+
 all: chopper 
 
 chopper: $(CHOPPER_SOURCES)
--- chopper.cpp
+++ chopper.cpp
@@ -1,44 +1,57 @@
 #include "PZdabFile.h"
+#include "PZdabWriter.h"
 #include <string>
 #include <iostream>
 
 int main(){
-    std::cerr << "Hi" << std::endl;
-    std::string filename = "~/sno.zdab";
-    FILE* f = fopen(filename.c_str(), "rb");
+    std::string infilename = "/home/cp/klabe/sno.zdab";
+    FILE* infile = fopen(infilename.c_str(), "rb");
 
     PZdabFile* p = new PZdabFile();
-    p->Init(f); 
+    if (p->Init(infile) < 0){
+        std::cerr << "Did not open file" << std::endl;
+        return -1;
+    }
 
-    int time0 = -1;
+    double time0 = -1;
     const double chunksize = 1.2; // Chunk Size in Seconds;
-    const int ticks = chunksize*50000000;
+    const double ticks = chunksize*50000000;
     const int maxtime = 8796093022208; // IS THIS CORRECT?
+    double time = 0;
     int index = 0;
+
+    //char *outfilename = "/home/cp/klabe/chopped.zdab";
+    //PZdabWriter w = PZdabWriter(outfilename,0);
 
     while(1){
         nZDAB* data = p->NextRecord();
-        PmtEventRecord* hits = reinterpret_cast<PmtEventRecord*>(p->GetBank(data));
-        //  PmtEventRecord* hits = PZdabFile::GetPmtRecord(data);
-        double time = get50MHzTime(hits);
-
-        if ( time0 == -1 )
-            time0 = time;
-
-        // OUTPUT ZDAB EVENT HERE
-
-        if (time0+ticks < maxtime){
-            if (time > time0+ticks)
-                //START NEW FILE
-                //WRITE TO DATABASE
-                index++;
+            if (data == NULL)
+                break;
+        PmtEventRecord* hits = p->GetPmtRecord(data);
+        if (hits != NULL){
+            time = get50MHzTime(hits);
+            if (time0 == -1)
+                time0 = time;
         }
-        else{
-            if (time > time0+ticks - maxtime && time < time0 ){
-                // DO YOUR THING
+        // OUTPUT ZDAB RECORD HERE
+        if (time0 + ticks < maxtime){
+            if (time > time0 + ticks){
+                // START NEW FILE, CLOSE PRESENT FILE
+                // WRITE TO DATABASE
+                index++;
+                time0 = time;
             }
         }
-
-        std::cout << time;
+        else{
+            if (time > time0 + ticks - maxtime && time < time0 ){
+                // START NEW FILE, CLOSE PRESENT FILE
+                // WRITE TO DATABASE
+                index ++;
+                time0 = time;
+            }
+        }
+        
+        std::cerr << time << std::endl;
     }
+    return 0;
 }
