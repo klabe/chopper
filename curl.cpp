596f67bfd488beecacfb75d6a9be24303db6b8b5
Kevin Labe <klabe@uchicago.edu>
2014-09-26 15:16:26 -0500

    Have curl accept only 10 (non-Error) alarms per second.

--- curl.cpp
+++ curl.cpp
@@ -8,16 +8,42 @@
 #include <stdlib.h>
 
 static CURL* curl; // curl connection object
+static const int max = 10; // maximum number of curl messages allowed per second
+static int n = 0; // number of curl messages in last second
+static int overflow = 0; // number of overfow messages
+static int oldwalltime = 0;
 
 // This function sends alarms to the monitoring website
 void alarm(const int level, const char* msg){
-  char curlmsg[256];
-  sprintf(curlmsg, "name=L2-client&level=%d&message=%s", level, msg);
-  curl_easy_setopt(curl, CURLOPT_POSTFIELDS, curlmsg);
-  curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, (long) strlen(curlmsg));
-  CURLcode res = curl_easy_perform(curl);
-  if(res != CURLE_OK)
-    fprintf(stderr, "Logging failed: %s\n", curl_easy_strerror(res));
+  int walltime = time(NULL);
+  if(walltime != oldwalltime){
+    if(overflow){
+      char mssg[128];
+      sprintf(mssg, "ERROR OVERFLOW: %d messages skipped", overflow);
+      char curlmsg[256];
+      sprintf(curlmsg, "name=L2-client&level=30&message=%s", mssg);
+      curl_easy_setopt(curl, CURLOPT_POSTFIELDS, curlmsg);
+      curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, (long) strlen(curlmsg));
+      CURLcode res = curl_easy_perform(curl);
+      if(res != CURLE_OK)
+        fprintf(stderr, "Logging failed: %s\n", curl_easy_strerror(res));
+    }
+    oldwalltime = walltime;
+    n = 0;
+    overflow = 0;
+  }
+  n++;
+  if(n > max && level < 40) 
+    overflow++;
+  else{
+    char curlmsg[256];
+    sprintf(curlmsg, "name=L2-client&level=%d&message=%s", level, msg);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, curlmsg);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, (long) strlen(curlmsg));
+    CURLcode res = curl_easy_perform(curl);
+    if(res != CURLE_OK)
+      fprintf(stderr, "Logging failed: %s\n", curl_easy_strerror(res));
+  }
 }
 
 // This function opens a curl connection
