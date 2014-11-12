cbf928fc16ba99bd6ebd2c62a4a48bf65f2f50e8
Kevin Labe <klabe@uchicago.edu>
2014-11-12 13:44:04 -0600

    Fix redis naming conventions

--- redis.cpp
+++ redis.cpp
@@ -70,25 +70,25 @@ void Writetoredis(l2stats stat, const int time){
     if(!reply)
       alarm(30, message);
 
-    reply = redisCommand(redis, "INCRBY ts:%d:%d:L2:gtid %d", intervals[i], ts, stat.gtid);
+    reply = redisCommand(redis, "INCRBY ts:%d:%d:l2:gtid %d", intervals[i], ts, stat.gtid);
     if(!reply)
       alarm(30, message);
-    reply = redisCmomand(redis, "EXPIRE ts:%d:%d:L2:gtid %d", intervals[i], ts, 2400*intervals[i]);
+    reply = redisCmomand(redis, "EXPIRE ts:%d:%d:l2:gtid %d", intervals[i], ts, 2400*intervals[i]);
     if(!reply)
       alarm(30, message);
 
-    reply = redisCommand(redis, "INCRBY ts:%d:%d:L2:run %d", intervals[i], ts, stat.run);
+    reply = redisCommand(redis, "INCRBY ts:%d:%d:l2:run %d", intervals[i], ts, stat.run);
     if(!reply)
       alarm(30, message);
-    reply = redisCommand(redis, "EXPIRE ts:%d:%d:L2:run %d", intervals[i], ts, 2400*intervals[i]);
+    reply = redisCommand(redis, "EXPIRE ts:%d:%d:l2:run %d", intervals[i], ts, 2400*intervals[i]);
     if(!reply)
       alarm(30, message);
 
     if(stat.burstbool){
-      reply = redisCommand(redis, "SET ts:%d:id:%d:Burst 1", intervals[i], ts);
+      reply = redisCommand(redis, "INCRBY ts:%d:id:%d:BURSTS 1", intervals[i], ts);
       if(!reply)
         alarm(30, message);
-      reply = redisCommand(redis, "EXPIRE ts:%d:id:%d:Burst", intervals[i], ts, 2400*intervals[i]);
+      reply = redisCommand(redis, "EXPIRE ts:%d:id:%d:BURSTS", intervals[i], ts, 2400*intervals[i]);
       if(!reply)
         alarm(30, message);
     }
